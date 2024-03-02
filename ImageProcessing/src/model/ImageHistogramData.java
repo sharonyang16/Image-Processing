963300cc7b27236e6d14abbadc12fdd8bb0a7990
Sharon Yang <sharonyyang0@gmail.com>
2024-03-02 10:21:22 -0500

    File structure changes pt.2

--- ImageProcessing/src/model/ImageHistogramData.java
+++
@@ -1,8 +0,0 @@
-package model;
-
-public interface ImageHistogramData {
-  int getNumRedAtValue(int value) throws IllegalArgumentException;
-  int getNumGreenAtValue(int value) throws IllegalArgumentException;
-  int getNumBlueAtValue(int value) throws IllegalArgumentException;
-  int getNumIntensityAtValue(int value) throws IllegalArgumentException;
-}
